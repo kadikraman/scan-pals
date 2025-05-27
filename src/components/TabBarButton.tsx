62e2287b7e640b6eb4b21c18af057047b7b0fa7e
Kadi Kraman <kadi@expo.io>
2025-05-27 14:54:08 +0200

    Animate the bottom tab icons

--- src/components/TabBarButton.tsx
+++ src/components/TabBarButton.tsx
@@ -1,20 +1,40 @@
 import * as Haptics from "expo-haptics";
 import { PropsWithChildren } from "react";
 import { GestureResponderEvent, Pressable } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 export function TabBarButton({
   onPress,
   children,
 }: PropsWithChildren<{ onPress?: (e: GestureResponderEvent) => void }>) {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
   return (
-    <Pressable
+    <AnimatedPressable
       className="flex-1 justify-center items-center"
       onPress={(e) => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         onPress?.(e);
       }}
+      onPressIn={() => {
+        scale.value = withSpring(0.9);
+      }}
+      onPressOut={() => {
+        scale.value = withSpring(1);
+      }}
+      style={animatedStyle}
     >
       {children}
-    </Pressable>
+    </AnimatedPressable>
   );
 }
