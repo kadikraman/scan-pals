8767835d78cd3a982ec8ce2c330ae48b64c04d93
Kadi Kraman <kadi@expo.io>
2025-05-27 16:07:53 +0200

    Add swipe to delete

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -5,6 +5,8 @@ import { Text } from "@/src/components/Text";
 import { Image } from "expo-image";
 import { avatars, fallbackAvatar } from "../constants";
 import { useTheme } from "../utils/useTheme";
+import { Deletable } from "./Deletable";
+import { useProfileStore } from "../store/profileStore";
 
 export function ProfileCard({
   profile,
@@ -21,46 +23,50 @@ export function ProfileCard({
 
   const tintColor = profile.color || accentColor;
 
+  const { deleteProfile } = useProfileStore();
+
   return (
-    <View
-      className={cn(
-        "bg-light-surface dark:bg-dark-surface web:rounded-lg",
-        "p-4 flex-row items-center gap-4",
-        {
-          "rounded-lg px-4 mx-4 elevation-sm shadow-sm mb-6": asCard,
-        },
-        className,
-      )}
-    >
-      <Image
-        source={
-          avatars.find((avatar) => avatar.id === profile.avatar)?.image ||
-          fallbackAvatar
-        }
-        style={{
-          width: 50,
-          height: 50,
-        }}
-        tintColor={tintColor}
-        contentFit="contain"
-      />
-      <View className="flex-1 mt-3">
-        <Text
-          large2x
-          bold
-          ellipsis
-          numberOfLines={1}
-          style={{ color: tintColor }}
-        >
-          {profile?.name}
-        </Text>
-        <Text bold numberOfLines={1}>
-          {profile?.bio}
-        </Text>
-        <Text secondary className="text-right" small>
-          {new Date(profile.updatedAt).toLocaleString()}
-        </Text>
+    <Deletable canDelete={canDelete} onDelete={() => deleteProfile(profile)}>
+      <View
+        className={cn(
+          "bg-light-surface dark:bg-dark-surface web:rounded-lg r",
+          "p-4 flex-row items-center gap-4",
+          {
+            "rounded-lg px-4 mx-4 elevation-sm shadow-sm mb-6": asCard,
+          },
+          className,
+        )}
+      >
+        <Image
+          source={
+            avatars.find((avatar) => avatar.id === profile.avatar)?.image ||
+            fallbackAvatar
+          }
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          tintColor={tintColor}
+          contentFit="contain"
+        />
+        <View className="flex-1 mt-3">
+          <Text
+            large2x
+            bold
+            ellipsis
+            numberOfLines={1}
+            style={{ color: tintColor }}
+          >
+            {profile?.name}
+          </Text>
+          <Text bold numberOfLines={1}>
+            {profile?.bio}
+          </Text>
+          <Text secondary className="text-right" small>
+            {new Date(profile.updatedAt).toLocaleString()}
+          </Text>
+        </View>
       </View>
-    </View>
+    </Deletable>
   );
 }
